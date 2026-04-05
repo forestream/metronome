5e24be196986add058024612a13c9df8f5c0f078
hanbin <rj2303@naver.com>
2026-04-05 17:45:35 +0900

    prototype

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,90 @@
-import Image from "next/image";
+"use client";
+
+import { useEffect, useRef, useState } from "react";
 
 export default function Home() {
+  const [bpm, setBpm] = useState(120);
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  const handleBpmChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const nextBpm = Math.min(240, Math.max(1, Number(event.target.value)));
+    setBpm(nextBpm);
+  };
+
+  const handlePlay = () => {
+    setIsPlaying((prev) => !prev);
+  };
+
+  const start = useRef<number | null>(null);
+  const lightRef = useRef<HTMLDivElement>(null);
+  const flickerRef = useRef<number>(0);
+  const raf = useRef<number | null>(null);
+  const hz = useRef<number>(0);
+  useEffect(() => {
+    const step: FrameRequestCallback = (time) => {
+      if (!isPlaying) {
+        if (raf.current) {
+          cancelAnimationFrame(raf.current);
+          raf.current = null;
+          flickerRef.current = 0;
+          start.current = null;
+          if (lightRef.current) {
+            lightRef.current.style.opacity = "0";
+          }
+        }
+        return;
+      }
+
+      if (start.current === null) {
+        start.current = time;
+      }
+      const elapsed = time - start.current;
+
+      if (elapsed < 1000) hz.current += 1;
+
+      flickerRef.current = Math.sin(
+        ((elapsed / 1000) * 2 * Math.PI * bpm) / 60,
+      );
+      if (lightRef.current) {
+        lightRef.current.style.opacity = `${flickerRef.current}`;
+      }
+
+      raf.current = requestAnimationFrame(step);
+    };
+
+    raf.current = requestAnimationFrame(step);
+  }, [bpm, isPlaying]);
+
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
+    <div className="mx-auto max-w-[800px] px-4 w-full min-h-dvh">
+      <div className="w-24 h-24 border border-amber-600 rounded my-24 mx-auto">
+        <div
+          ref={lightRef}
+          className="w-full h-full bg-amber-600"
+          style={{ opacity: "0" }}
         />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+      </div>
+      <div className="flex flex-col gap-2 items-center justify-center w-full">
+        <div className="flex gap-2 w-full items-center">
+          <label htmlFor="bpm">BPM</label>
+          <input
+            className="border border-amber-600 rounded-md w-full py-2 text-2xl"
+            id="bpm"
+            type="number"
+            value={bpm}
+            onChange={handleBpmChange}
+            min={1}
+            max={240}
+            step={1}
+          />
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+        <button
+          onClick={handlePlay}
+          className="bg-amber-600 text-white rounded-md w-full py-2 text-2xl"
+        >
+          {isPlaying ? "Stop" : "Play"}
+        </button>
+      </div>
     </div>
   );
 }
